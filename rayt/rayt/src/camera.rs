ba6b3c6a3df3ff47973d569047554adad0e4d02e
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-08-14 00:53:28 +0900

    [rayt] Add rayt lib

---
+++ rayt/rayt/src/camera.rs
@@ -0,0 +1,78 @@
+// BSD 2-Clause License
+//
+// Copyright (c) 2021, mebiusbox
+// Copyright (c) 2022, Takahiro Ueno
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+use crate::{Point3, Ray, Vec3};
+
+#[derive(Debug)]
+pub struct Camera {
+    pub origin: Point3,
+    pub u: Vec3,
+    pub v: Vec3,
+    pub w: Vec3,
+}
+
+impl Camera {
+    pub fn new(u: Vec3, v: Vec3, w: Vec3) -> Self {
+        Self {
+            origin: Vec3::zero(),
+            u,
+            v,
+            w,
+        }
+    }
+
+    pub fn from_lookat(
+        origin: Vec3,
+        lookat: Vec3,
+        viewup: Vec3,
+        viewfov: f64,
+        aspect: f64,
+    ) -> Self {
+        let z = (origin - lookat).normalize();
+        let x = viewup.cross(z).normalize();
+        let y = z.cross(x).normalize();
+
+        // fov(field of view)
+        let halfh = (viewfov * 0.5).tan();
+        let halfw = halfh * aspect;
+
+        Self {
+            origin,
+            u: 2.0 * halfw * x,
+            v: 2.0 * halfh * y,
+            w: origin - z - halfw * x - halfh * y,
+        }
+    }
+
+    pub fn ray(&self, u: f64, v: f64) -> Ray {
+        Ray {
+            origin: self.origin,
+            direction: self.w + u * self.u + v * self.v,
+        }
+    }
+}
---
+++ rayt/rayt/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod float3;
+pub use self::float3::{Color, Float3, Point3, Vec3};
+
+pub mod quat;
+pub use self::quat::Quat;
+
+pub mod ray;
+pub use self::ray::Ray;
+
+pub mod camera;
+pub use self::camera::Camera;
+
+pub use std::f64::consts::{FRAC_1_PI, PI};
+pub const PI2: f64 = PI * 2.0;
+pub const EPS: f64 = 1e-6;
