cdbad78739f993bdced1ce1e779e4c5a24c5cbb9
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-09-17 18:17:08 +0900

    [binary-loader] Change location of class definitions

--- binary-loader/loader.h
+++ binary-loader/loader.h
@@ -7,6 +7,48 @@ class Binary;
 class Section;
 class Symbol;
 
+class Symbol {
+   public:
+    enum SymbolType {
+        SYM_TYPE_UNKOWN,
+        SYM_TYPE_FUNC,
+    };
+
+    Symbol() : name(), type(SYM_TYPE_UNKOWN), addr(0) {}
+
+    std::string name;
+    SymbolType type;
+    uint64_t addr;
+};
+
+class Section {
+   public:
+    enum SectionType {
+        SEC_TYPE_NONE,
+        SEC_TYPE_CODE,
+        SEC_TYPE_DATA,
+    };
+
+    Section()
+        : name(),
+          type(SEC_TYPE_NONE),
+          binary(nullptr),
+          virtual_memory_addrres(0),
+          size(0),
+          bytes(nullptr) {}
+
+    bool contains(uint64_t addr) {
+        return (virtual_memory_addrres <= addr) && (addr - virtual_memory_addrres < size);
+    }
+
+    std::string name;
+    SectionType type;
+    Binary *binary;
+    uint64_t virtual_memory_addrres;
+    uint64_t size;
+    uint8_t *bytes;
+};
+
 class Binary {
    public:
     enum BinaryType {
@@ -38,48 +80,6 @@ class Binary {
     uint64_t entrypoint;
     std::vector<Section> sections;
     std::vector<Symbol> symbols;
-};
-
-class Section {
-   public:
-    enum SectionType {
-        SEC_TYPE_NONE,
-        SEC_TYPE_CODE,
-        SEC_TYPE_DATA,
-    };
-
-    Section()
-        : name(),
-          type(SEC_TYPE_NONE),
-          binary(nullptr),
-          virtual_memory_addrres(0),
-          size(0),
-          bytes(nullptr) {}
-
-    bool contains(uint64_t addr) {
-        return (virtual_memory_addrres <= addr) && (addr - virtual_memory_addrres < size);
-    }
-
-    std::string name;
-    SectionType type;
-    Binary *binary;
-    uint64_t virtual_memory_addrres;
-    uint64_t size;
-    uint8_t *bytes;
-};
-
-class Symbol {
-   public:
-    enum SymbolType {
-        SYM_TYPE_UNKOWN,
-        SYM_TYPE_FUNC,
-    };
-
-    Symbol() : name(), type(SYM_TYPE_UNKOWN), addr(0) {}
-
-    std::string name;
-    SymbolType type;
-    uint64_t addr;
 };
 
 int load_binary(std::string &filename, Binary *binary, Binary::BinaryType type);
