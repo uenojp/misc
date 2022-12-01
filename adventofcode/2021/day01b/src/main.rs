b043887e793670679529d2728fee5f4e93983928
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-12-01 19:44:07 +0900

    [adventofcode] day01

---
+++ adventofcode/2021/day01b/src/main.rs
@@ -0,0 +1,12 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let measurements = include_str!("../input")
+        .lines()
+        .map(|s| s.parse::<u64>())
+        .collect::<Result<Vec<u64>, _>>()?;
+
+    let count = measurements.windows(4).filter(|w| w[0] < w[3]).count();
+
+    println!("{:?}", count);
+
+    Ok(())
+}
