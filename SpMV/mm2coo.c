afb944580a22ccc05381a084429f86a60f8be38b
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2023-03-06 10:27:33 +0900

    [SpMV] Refactor

--- SpMV/mm2coo.c
+++ SpMV/mm2coo.c
@@ -78,7 +78,7 @@ int read_mm_header(FILE* fp, uint32_t* nptr, uint32_t* nnzptr) {
     return 0;
 }
 
-int read_mm_data(FILE* fp, uint32_t** coo_row_index, uint32_t** coo_col_index, double** coo_value,
+int read_mm_data(FILE* fp, uint32_t* coo_row_index, uint32_t* coo_col_index, double* coo_value,
                  uint32_t nnz) {
     char* line = NULL;
     size_t len = 0;
@@ -110,9 +110,10 @@ int read_mm_data(FILE* fp, uint32_t** coo_row_index, uint32_t** coo_col_index, d
 
         assert(i - 1 >= 0 && j - 1 >= 0);
 
-        (*coo_row_index)[index] = i - 1;
-        (*coo_col_index)[index] = j - 1;
-        (*coo_value)[index] = value;
+        // 0-based indexing
+        coo_row_index[index] = i - 1;
+        coo_col_index[index] = j - 1;
+        coo_value[index] = value;
 
         index++;
     }
@@ -124,8 +125,8 @@ int read_mm_data(FILE* fp, uint32_t** coo_row_index, uint32_t** coo_col_index, d
     }
 }
 
-int mm2coo(const char* pathname, uint32_t** coo_row_index, uint32_t** coo_col_index,
-           double** coo_value, uint32_t* nptr, uint32_t* nnzptr) {
+int mm2coo(const char* pathname, uint32_t** coo_row_index_ptr, uint32_t** coo_col_index_ptr,
+           double** coo_value_ptr, uint32_t* nptr, uint32_t* nnzptr) {
     FILE* fp = NULL;
 
     if ((fp = fopen(pathname, "r")) == NULL) {
@@ -142,14 +143,14 @@ int mm2coo(const char* pathname, uint32_t** coo_row_index, uint32_t** coo_col_in
     uint32_t n = *nptr;
     uint32_t nnz = *nnzptr;
 
-    *coo_row_index = (uint32_t*)malloc((size_t)nnz * sizeof(uint32_t));
-    *coo_col_index = (uint32_t*)malloc((size_t)nnz * sizeof(uint32_t));
-    *coo_value = (double*)malloc((size_t)nnz * sizeof(double));
-    if (!(*coo_row_index) || !(*coo_col_index) || !(*coo_value)) {
+    *coo_row_index_ptr = (uint32_t*)malloc((size_t)nnz * sizeof(uint32_t));
+    *coo_col_index_ptr = (uint32_t*)malloc((size_t)nnz * sizeof(uint32_t));
+    *coo_value_ptr = (double*)malloc((size_t)nnz * sizeof(double));
+    if (!(*coo_row_index_ptr) || !(*coo_col_index_ptr) || !(*coo_value_ptr)) {
         return -1;
     }
 
-    if (read_mm_data(fp, coo_row_index, coo_col_index, coo_value, nnz) < 0) {
+    if (read_mm_data(fp, *coo_row_index_ptr, *coo_col_index_ptr, *coo_value_ptr, nnz) < 0) {
         fprintf(stderr, "invalid Matrix Market data\n");
         fclose(fp);
         return -1;
@@ -163,6 +164,6 @@ int mm2coo(const char* pathname, uint32_t** coo_row_index, uint32_t** coo_col_in
 void print_coo(const uint32_t* coo_row_index, const uint32_t* coo_col_index, double* coo_value,
                uint32_t nnz) {
     for (uint32_t i = 0; i < nnz; i++) {
-        printf("%d %d %lf\n", coo_row_index[i], coo_col_index[i], coo_value[i]);
+        printf("%d %d %.13e\n", coo_row_index[i], coo_col_index[i], coo_value[i]);
     }
 }
