c8effe8436c104451dcd634e4a5fd66a07a6dbb8
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2023-03-07 06:19:04 +0900

    [SpMV] Impl matrix product

--- SpMV/main.c
+++ SpMV/main.c
@@ -7,6 +7,9 @@ int main(int argc, char** argv) {
         exit(0);
     }
 
+    //
+    // read MatrixMarket Format
+    //
     uint32_t n = 0;
     uint32_t nnz = 0;
     uint32_t* coo_row_index = NULL;
@@ -21,6 +24,24 @@ int main(int argc, char** argv) {
     // printf("n: %d, nnz: %d\n", n, nnz);
     // print_coo(coo_row_index, coo_col_index, coo_value, nnz);
 
+    // matrix product with coo
+    double* x1 = (double*)malloc((size_t)n * sizeof(double));
+    double* y1 = (double*)malloc((size_t)n * sizeof(double));
+
+    for (uint32_t i = 0; i < n; i++) {
+        x1[i] = (double)(i * i);
+        y1[i] = 0.0;
+    }
+    for (uint32_t i = 0; i < nnz; i++) {
+        y1[coo_row_index[i]] += coo_value[i] * x1[coo_col_index[i]];
+    }
+    for (uint32_t i = 0; i < n; i++) {
+        printf("%lf%c", y1[i], i == n - 1 ? '\n' : ' ');
+    }
+
+    //
+    // coo to csr
+    //
     uint32_t* csr_row_ptr = NULL;
     uint32_t* csr_col_index = NULL;
     double* csr_value = NULL;
@@ -32,5 +53,22 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    print_csr(csr_row_ptr, csr_col_index, csr_value, n);
+    // print_csr(csr_row_ptr, csr_col_index, csr_value, n);
+
+    // matrix product with csr
+    double* x2 = (double*)malloc((size_t)n * sizeof(double));
+    double* y2 = (double*)malloc((size_t)n * sizeof(double));
+
+    for (uint32_t i = 0; i < n; i++) {
+        x2[i] = (double)(i * i);
+        y2[i] = 0.0;
+    }
+    for (uint32_t i = 0; i < n; i++) {
+        for (uint32_t j = csr_row_ptr[i]; j < csr_row_ptr[i + 1]; j++) {
+            y2[i] += csr_value[j] * x2[csr_col_index[j]];
+        }
+    }
+    for (uint32_t i = 0; i < n; i++) {
+        printf("%lf%c", y2[i], i == n - 1 ? '\n' : ' ');
+    }
 }
