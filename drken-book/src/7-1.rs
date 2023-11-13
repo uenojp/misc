ff9266fc021b1db49921f605bd2ec0a1509fab83
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2023-11-13 16:54:28 +0900

    [drken-book] chap7

--- drken-book/src/7-1.rs
+++ drken-book/src/7-1.rs
@@ -3,7 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [i64; n],
-        b: [i64; n],
+        mut a: [i64; n],
+        mut b: [i64; n],
     }
+    a.sort();
+    b.sort();
+
+    let mut i = 0;
+    for bi in b {
+        if a[i] < bi {
+            i += 1;
+        }
+    }
+    print!("{}", i);
 }
---
+++ drken-book/src/7-abc131d.rs
@@ -0,0 +1,8 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        mut ab: [(u64, u64); n],
+    }
+}
