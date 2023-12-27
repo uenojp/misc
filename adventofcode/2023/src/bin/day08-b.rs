0041fcfb0edadd74a52407dff1af87f79bf5f752
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2023-12-27 23:27:39 +0900

    [adventofcode] 2023 day08 wip

---
+++ adventofcode/2023/src/bin/day08-b.rs
@@ -0,0 +1,63 @@
+use std::{
+    collections::HashMap,
+    io::{self, BufRead},
+};
+
+type Node = String;
+
+#[derive(Debug)]
+struct Simulator {
+    routes: HashMap<Node, (Node, Node)>,
+}
+
+impl Simulator {
+    fn new(lines: impl Iterator<Item = String>) -> Self {
+        Self {
+            routes: lines.map(|line| parse(&line)).collect::<HashMap<_, _>>(),
+        }
+    }
+
+    fn simulate(&self, navigation: &str) -> u64 {
+        let mut step = 0;
+
+        let mut state = "AAA";
+
+        for instruction in navigation.chars().cycle() {
+            match instruction {
+                'L' => state = &self.routes[state].0,
+                'R' => state = &self.routes[state].1,
+                _ => unreachable!("unexpected instruction {}", instruction),
+            }
+            step += 1;
+
+            if state == "ZZZ" {
+                break;
+            }
+        }
+
+        step
+    }
+}
+
+fn parse(line: &str) -> (Node, (Node, Node)) {
+    let (from, to) = line
+        .split_once(" = ")
+        .map(|(from, to)| {
+            let (left, right) = to
+                .trim_matches(|c| c == '(' || c == ')')
+                .split_once(", ")
+                .unwrap();
+            (from, (left, right))
+        })
+        .unwrap();
+
+    (String::from(from), (String::from(to.0), String::from(to.1)))
+}
+
+fn main() {
+    let mut lines = io::stdin().lock().lines().map(Result::unwrap);
+    let navigation = lines.next().unwrap();
+    lines.next();
+    let simulator = Simulator::new(lines);
+    println!("{}", simulator.simulate(&navigation));
+}
